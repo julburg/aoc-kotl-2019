d1db2abcbcfb7b4c4f9a4967d16600b1ac0250fe
Julia Burgard <burgard@synyx.de>
2019-12-15 13:41:49 +0100

    Fix solution for day 10

--- src/main/kotl/de/aoc/Day10.kt
+++ src/main/kotl/de/aoc/Day10.kt
@@ -3,46 +3,69 @@ package de.aoc
 import java.awt.Point
 
 
-fun day10(map: String): Int {
-    val starMap = parseMap(map)
-    val hiddenPositions = HashMap<Point, Set<Point>>()
-    for (starPoint in starMap.positions) {
-        hiddenPositions.put(starPoint, starMap.hiddenPositions(starPoint))
-    }
-
-    val pointWithMinHiddenPositions = hiddenPositions.minBy { it.value.size }!!
-    return starMap.positions.size - pointWithMinHiddenPositions.value.size;
+fun day10a(map: String): Int {
+    val seenPositions = getSeenPositions(map)
+    return seenPositions.values.map { it.size }.max()!!;
 }
 
-data class AsteroidMap(val positions: List<Point>) {
+fun day10b(map: String): Int {
+    val seenPositions = getSeenPositions(map)
+    val monitoringStation = seenPositions.maxBy { it.value.size }
 
-    fun maxX(): Int {
-        return positions.map { it.x }.max()!!
+    val pointsSeenByStation = monitoringStation!!.value
+    val stationPosition = monitoringStation.key
+    println("Station: " + stationPosition)
+    val sortedPositionsByAngle = pointsSeenByStation.sortedBy { calcRotationAngleInDegrees(stationPosition,it) }
+
+    val twoHundrethPosition = sortedPositionsByAngle.get(199)
+    return twoHundrethPosition.x * 100+ twoHundrethPosition.y
+}
+
+//copied from stackoverflow
+fun calcRotationAngleInDegrees(centerPt: Point, targetPt: Point): Double {
+    var theta = Math.atan2((targetPt.y - centerPt.y).toDouble(), (targetPt.x - centerPt.x).toDouble())
+    theta += Math.PI / 2.0
+    var angle = Math.toDegrees(theta)
+    if (angle < 0) {
+        angle += 360.0
     }
 
-    fun maxY(): Int {
-        return positions.map { it.y }.max()!!
-    }
+    return angle
+}
 
-    fun getHiddenPositions(starPoint: Point, pointToCheck: Point): List<Point> {
+private fun getSeenPositions(map: String): HashMap<Point, Set<Point>> {
+    val starMap = parseMap(map)
+    val seenPositions = HashMap<Point, Set<Point>>()
+    for (starPoint in starMap.asteroidPositions) {
+        seenPositions.put(starPoint, starMap.seenPositions(starPoint))
+    }
+    return seenPositions
+}
+
+data class AsteroidMap(val asteroidPositions: List<Point>) {
+
+    fun getPositionsInBetween(starPoint: Point, pointToCheck: Point): List<Point> {
         if (isOnSameVerticalHorizontal(starPoint, pointToCheck)) {
-            return getAllPointsHorizontalOrVertical(starPoint, pointToCheck, positions)
-        }
-        if (isOnSameDiagonal(starPoint, pointToCheck)) {
-            return getAllPointsDiagonal(starPoint, pointToCheck, this)
+            return getAllPointsHorizontalOrVertical(starPoint, pointToCheck)
         }
 
-        return getAllPointsInBetween(starPoint, pointToCheck, this)
+        return getAllPointsInBetween(starPoint, pointToCheck)
     }
 
-    fun hiddenPositions(starPoint: Point): Set<Point> {
-        val hiddenPositions = HashSet<Point>()
-        for (pointToCheck in positions) {
-            if (starPoint != pointToCheck) {
-                hiddenPositions.addAll(getHiddenPositions(starPoint, pointToCheck).toSet())
+    fun seenPositions(starPoint: Point): Set<Point> {
+        val seenPositions = HashSet<Point>()
+        for (positionToCheck in asteroidPositions) {
+            if (starPoint != positionToCheck) {
+                var positionsInBetween = getPositionsInBetween(starPoint, positionToCheck)
+                positionsInBetween = positionsInBetween.minus(starPoint)
+                positionsInBetween = positionsInBetween.minus(positionToCheck)
+                val positionInBetweenWithAsteroids = positionsInBetween.filter { asteroidPositions.contains(it) }
+                if (positionInBetweenWithAsteroids.isEmpty()) {
+                    seenPositions.add(positionToCheck)
+                }
             }
         }
-        return hiddenPositions
+        return seenPositions
     }
 }
 
@@ -57,14 +80,8 @@ fun isOnSameVerticalHorizontal(point1: Point, point2: Point): Boolean {
 private fun isOnHorizontal(point1: Point, point2: Point) = point1.y == point2.y
 private fun isOnVertical(point1: Point, point2: Point) = point1.x == point2.x
 
-fun isOnSameDiagonal(point1: Point, point2: Point): Boolean {
-    if (Math.abs(point1.x - point2.x) == Math.abs(point1.y - point2.y)) {
-        return true
-    }
-    return false
-}
 
-fun getAllPointsHorizontalOrVertical(point1: Point, point2: Point, allPoints: List<Point>): List<Point> {
+fun getAllPointsHorizontalOrVertical(point1: Point, point2: Point): List<Point> {
     if (isOnHorizontal(point1, point2)) {
         if (point1.x > point2.x) {
             return (point2.x..point1.x).map { x -> Point(x, point2.y) }
@@ -77,55 +94,76 @@ fun getAllPointsHorizontalOrVertical(point1: Point, point2: Point, allPoints: Li
     return (point1.y..point2.y).map { y -> Point(point2.x, y) }
 }
 
-fun getAllPointsDiagonal(point1: Point, point2: Point, map: AsteroidMap): List<Point> {
-    if (rightBottom(point1, point2)) {
-        return (point1.x..point2.x).withIndex().map { (index, x) -> Point(x, point1.y + index) }
-    }
-    if (leftUp(point1, point2)) {
-        return (point2.x..point1.x).withIndex().map { (index, x) -> Point(x, point2.y + index) }
-    }
-    if (leftBottom(point1, point2)) {
-        return (point1.x..point2.x).withIndex().map { (index, x) -> Point(x, point1.y - index) }
-    }
-    if (rightUp(point1, point2)) {
-        return (point2.x..point1.x).withIndex().map { (index, x) -> Point(x, point2.y - index) }
-    }
-    throw IllegalArgumentException("Should not happen")
-}
 
 fun getManhattanDistance(point1: Point, point2: Point): Pair<Int, Int> {
     return Pair(Math.abs(point1.x - point2.x), Math.abs(point1.y - point2.y))
 }
 
-fun getLowestCommonDivisor(distance: Pair<Int, Int>):Int {
-    var kgt = 2
+fun getGreatestCommonDivisor(distance: Pair<Int, Int>): Int {
+    var kgt = Math.max(distance.first, distance.second)
     while (true) {
-        if (distance.first % kgt== 0 && kgt % distance.second % kgt== 0) {
+        if (distance.first % kgt == 0 && distance.second % kgt == 0) {
             return kgt
         }
-        ++kgt
+        --kgt
     }
 }
 
-fun getAllPointsInBetween(point1: Point, point2: Point, map: AsteroidMap): List<Point> {
+
+fun getAllPointsInBetween(point1: Point, point2: Point): List<Point> {
+    val manhattanDistance = smallestManhattanDistance(point1, point2)
+    val pointsInBetween = ArrayList<Point>()
     if (rightBottom(point1, point2)) {
+        var nextPoint = point1
+        while (nextPoint != point2) {
+            nextPoint = Point(nextPoint.x + manhattanDistance.first, nextPoint.y + manhattanDistance.second)
+            pointsInBetween.add(nextPoint)
+        }
+        return pointsInBetween
     }
     if (leftUp(point1, point2)) {
+        var nextPoint = point2
+        while (nextPoint != point1) {
+            nextPoint = Point(nextPoint.x + manhattanDistance.first, nextPoint.y + manhattanDistance.second)
+            pointsInBetween.add(nextPoint)
+        }
+        return pointsInBetween
     }
     if (rightUp(point1, point2)) {
+        var nextPoint = point1
+        while (nextPoint != point2) {
+            nextPoint = Point(nextPoint.x + manhattanDistance.first, nextPoint.y - manhattanDistance.second)
+            pointsInBetween.add(nextPoint)
+        }
+        return pointsInBetween
     }
     if (leftBottom(point1, point2)) {
+        var nextPoint = point2
+        while (nextPoint != point1) {
+            nextPoint = Point(nextPoint.x + manhattanDistance.first, nextPoint.y - manhattanDistance.second)
+            pointsInBetween.add(nextPoint)
+        }
+        return pointsInBetween
     }
     throw IllegalArgumentException("Should not happen")
 }
 
-private fun rightUp(point1: Point, point2: Point) = point1.x > point2.x && point1.y < point2.y
+fun smallestManhattanDistance(point1: Point, point2: Point): Pair<Int, Int> {
+    val manhattanDistance = getManhattanDistance(point1, point2)
+    val gcd = getGreatestCommonDivisor(manhattanDistance)
+    return Pair(manhattanDistance.first / gcd, manhattanDistance.second / gcd)
+}
 
-private fun leftBottom(point1: Point, point2: Point) = point1.x < point2.x && point1.y > point2.y
-
+private fun rightUp(point1: Point, point2: Point) = point1.x < point2.x && point1.y > point2.y
+private fun leftBottom(point1: Point, point2: Point) = point1.x > point2.x && point1.y < point2.y
 private fun leftUp(point1: Point, point2: Point) = point1.x > point2.x && point1.y > point2.y
-
 private fun rightBottom(point1: Point, point2: Point) = point1.x < point2.x && point1.y < point2.y
+
+
+private fun rightUpEquals(point1: Point, point2: Point) = point1.x <= point2.x && point1.y > point2.y
+private fun rightBottomEquals(point1: Point, point2: Point) = point1.x < point2.x && point1.y <= point2.y
+private fun leftBottomEquals(point1: Point, point2: Point) = point1.x >= point2.x && point1.y < point2.y
+private fun leftUpEquals(point1: Point, point2: Point) = point1.x > point2.x && point1.y >= point2.y
 
 
 fun parseMap(map: String): AsteroidMap {
--- src/test/kotl/de/aoc/Day10KtTest.kt
+++ src/test/kotl/de/aoc/Day10KtTest.kt
@@ -2,6 +2,7 @@ package de.aoc
 
 import org.junit.Test
 import java.awt.Point
+import java.io.File
 import kotlin.test.assertEquals
 
 /**
@@ -9,14 +10,151 @@ import kotlin.test.assertEquals
  */
 class Day10KtTest {
 
+
+
+    @Test
+    fun day10bExampleOne() {
+        val map = ".#..##.###...#######\n" +
+                "##.############..##.\n" +
+                ".#.######.########.#\n" +
+                ".###.#######.####.#.\n" +
+                "#####.##.#.##.###.##\n" +
+                "..#####..#.#########\n" +
+                "####################\n" +
+                "#.####....###.#.#.##\n" +
+                "##.#################\n" +
+                "#####.##.###..####..\n" +
+                "..######..##.#######\n" +
+                "####.##.####...##..#\n" +
+                ".#####..#.######.###\n" +
+                "##...#.##########...\n" +
+                "#.##########.#######\n" +
+                ".####.#.###.###.#.##\n" +
+                "....##.##.###..#####\n" +
+                ".#.#.###########.###\n" +
+                "#.#.#.#####.####.###\n" +
+                "###.##.####.##.#..##"
+        val result = day10b(map)
+
+        assertEquals(802, result)
+    }
+
+    @Test
+    fun day10aTest() {
+        val map = File("inputDay10").inputStream().bufferedReader().use { it.readText() }
+        val result = day10a(map)
+
+        assertEquals(214, result)
+    }
+
+
+    @Test
+    fun day10bTest() {
+        val map = File("inputDay10").inputStream().bufferedReader().use { it.readText() }
+        val result = day10b(map)
+
+        assertEquals(502, result)
+    }
+
+    @Test
+    fun day10aExampleOne() {
+        val map = ".#..#\n" +
+                ".....\n" +
+                "#####\n" +
+                "....#\n" +
+                "...##"
+        val result = day10a(map)
+
+        assertEquals(8, result)
+    }
+
+    @Test
+    fun day10aExampleTwo() {
+        val map = "......#.#.\n" +
+                "#..#.#....\n" +
+                "..#######.\n" +
+                ".#.#.###..\n" +
+                ".#..#.....\n" +
+                "..#....#.#\n" +
+                "#..#....#.\n" +
+                ".##.#..###\n" +
+                "##...#..#.\n" +
+                ".#....####"
+        val result = day10a(map)
+
+        assertEquals(33, result)
+    }
+
+    @Test
+    fun day10aExampleThree() {
+        val map = "#.#...#.#.\n" +
+                ".###....#.\n" +
+                ".#....#...\n" +
+                "##.#.#.#.#\n" +
+                "....#.#.#.\n" +
+                ".##..###.#\n" +
+                "..#...##..\n" +
+                "..##....##\n" +
+                "......#...\n" +
+                ".####.###."
+        val result = day10a(map)
+
+        assertEquals(35, result)
+    }
+
+    @Test
+    fun day10aExampleFour() {
+        val map = ".#..#..###\n" +
+                "####.###.#\n" +
+                "....###.#.\n" +
+                "..###.##.#\n" +
+                "##.##.#.#.\n" +
+                "....###..#\n" +
+                "..#.#..#.#\n" +
+                "#..#.#.###\n" +
+                ".##...##.#\n" +
+                ".....#.#.."
+        val result = day10a(map)
+
+        assertEquals(41, result)
+    }
+
+    @Test
+    fun day10aExampleFive() {
+        val map = ".#..##.###...#######\n" +
+                "##.############..##.\n" +
+                ".#.######.########.#\n" +
+                ".###.#######.####.#.\n" +
+                "#####.##.#.##.###.##\n" +
+                "..#####..#.#########\n" +
+                "####################\n" +
+                "#.####....###.#.#.##\n" +
+                "##.#################\n" +
+                "#####.##.###..####..\n" +
+                "..######..##.#######\n" +
+                "####.##.####...##..#\n" +
+                ".#####..#.######.###\n" +
+                "##...#.##########...\n" +
+                "#.##########.#######\n" +
+                ".####.#.###.###.#.##\n" +
+                "....##.##.###..#####\n" +
+                ".#.#.###########.###\n" +
+                "#.#.#.#####.####.###\n" +
+                "###.##.####.##.#..##"
+        val result = day10a(map)
+
+        assertEquals(210, result)
+    }
+
+
     @Test
     fun parseMap() {
         val map = ".#.\n" +
                 "..#\n" +
                 "##."
-        val asteroidMap = parseMap(map);
+        val asteroidMap = parseMap(map)
 
-        assertEquals(listOf(Point(1, 0), Point(2, 1), Point(0, 2), Point(1, 2)), asteroidMap.positions)
+        assertEquals(listOf(Point(1, 0), Point(2, 1), Point(0, 2), Point(1, 2)), asteroidMap.asteroidPositions)
     }
 
     @Test
@@ -32,67 +170,53 @@ class Day10KtTest {
     }
 
     @Test
-    fun isOnSameDiagonal() {
-        val onSameDiagonal = isOnSameDiagonal(Point(0, 0), Point(1, 1))
-        assertEquals(true, onSameDiagonal)
-
-        val notOnSameDiagonal = isOnSameDiagonal(Point(0, 0), Point(0, 1))
-        assertEquals(false, notOnSameDiagonal)
-    }
-
-    @Test
     fun getAllPointsHorizontalOrVerticalTest() {
-        val map = "###\n" +
-                "###\n" +
-                "###"
-        val mapPoints = parseMap(map)
 
-        var horizontalPoints = getAllPointsHorizontalOrVertical(Point(0, 0), Point(2, 0), mapPoints.positions)
+        var horizontalPoints = getAllPointsHorizontalOrVertical(Point(0, 0), Point(2, 0))
         assertEquals(listOf(Point(0, 0), Point(1, 0), Point(2, 0)), horizontalPoints)
 
-        horizontalPoints = getAllPointsHorizontalOrVertical(Point(3, 0), Point(1, 0), mapPoints.positions)
+        horizontalPoints = getAllPointsHorizontalOrVertical(Point(3, 0), Point(1, 0))
         assertEquals(listOf(Point(1, 0), Point(2, 0), Point(3, 0)), horizontalPoints)
 
-        val verticalPoints = getAllPointsHorizontalOrVertical(Point(0, 0), Point(0, 2), mapPoints.positions)
+        val verticalPoints = getAllPointsHorizontalOrVertical(Point(0, 0), Point(0, 2))
         assertEquals(listOf(Point(0, 0), Point(0, 1), Point(0, 2)), verticalPoints)
     }
 
     @Test
-    fun getAllPointsDiagonalTest() {
-        val map = "###\n" +
-                "###\n" +
-                "###"
-        val mapPoints = parseMap(map)
+    fun getManhattanDistanceTest() {
+        val (distanceX, distanceY) = getManhattanDistance(Point(2, 2), Point(5, 3))
 
-        var diagonalPoints = getAllPointsDiagonal(Point(0, 0), Point(2, 2), mapPoints)
-        assertEquals(listOf(Point(0, 0), Point(1, 1), Point(2, 2)), diagonalPoints)
+        assertEquals(3, distanceX)
+        assertEquals(1, distanceY)
 
-        diagonalPoints = getAllPointsDiagonal(Point(2, 2), Point(0, 0), mapPoints)
-        assertEquals(listOf(Point(0, 0), Point(1, 1), Point(2, 2)), diagonalPoints)
-
+        val smallestManhattanDistance = smallestManhattanDistance(Point(0, 0), Point(6, 3))
+        assertEquals(Pair(2, 1), smallestManhattanDistance)
     }
 
     @Test
-    fun getLowestCommonDivisorTest() {
-        var lowestCommonDivisor = getLowestCommonDivisor(Pair(4, 12));
-        assertEquals(2, lowestCommonDivisor)
+    fun getGreatestCommonDivisorTest() {
+        var greatestCommonDivisor = getGreatestCommonDivisor(Pair(8, 12))
+        assertEquals(4, greatestCommonDivisor)
 
-        lowestCommonDivisor = getLowestCommonDivisor(Pair(3, 18));
-        assertEquals(3, lowestCommonDivisor)
+        greatestCommonDivisor = getGreatestCommonDivisor(Pair(2, 3))
+        assertEquals(1, greatestCommonDivisor)
+
+        greatestCommonDivisor = getGreatestCommonDivisor(Pair(6, 9))
+        assertEquals(3, greatestCommonDivisor)
     }
+
 
     @Test
     fun getAllPointInBetween() {
-        val map = ".....\n" +
-                ".....\n" +
-                "....."
-        val mapPoints = parseMap(map)
 
-        var diagonalPoints = getAllPointsInBetween(Point(0, 0), Point(2, 1), AsteroidMap(emptyList()))
-        assertEquals(listOf(Point(2, 1), Point(4, 2)), diagonalPoints)
+        var diagonalPoints = getAllPointsInBetween(Point(0, 0), Point(6, 3))
+        assertEquals(listOf(Point(2, 1), Point(4, 2), Point(6, 3)), diagonalPoints)
 
-        diagonalPoints = getAllPointsInBetween(Point(4, 4), Point(2, 1), AsteroidMap(emptyList()))
-        assertEquals(listOf(Point(2, 1), Point(0, 0)), diagonalPoints)
+        diagonalPoints = getAllPointsInBetween(Point(4, 4), Point(2, 8))
+        assertEquals(listOf(Point(3, 6), Point(4, 4)), diagonalPoints)
+
+        diagonalPoints = getAllPointsInBetween(Point(-1, -2), Point(2, 7))
+        assertEquals(listOf(Point(0, 1), Point(1, 4), Point(2, 7)), diagonalPoints)
 
     }
 
